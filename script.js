c61b7b168a6cd0dcfebd6a6ef99bf5319b12d97b
kshitijvarshney5 <kshitijvarshney51@outlook.com>
2024-03-31 22:35:19 +0530

    changed f to c

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-
-
 let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
-
 
 const getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
-  const weatherPromise  = fetch(FULL_URL);
+  const weatherPromise = fetch(FULL_URL);
   return weatherPromise.then((response) => {
     return response.json();
-  })
-}
-
+  });
+};
 
 const searchCity = () => {
-  const city = document.getElementById('city-input').value;
+  const city = document.getElementById("city-input").value;
   getWeatherData(city)
-  .then((res)=>{
-    showWeatherData(res);
-  }).catch((error)=>{
-    console.log(error);
-    console.log("Something happend");
-  })
-}
-
+    .then((res) => {
+      console.log(res);
+      showWeatherData(res);
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log("Something happend");
+    });
+};
 
 showWeatherData = (weatherData) => {
   document.getElementById("city-name").innerText = weatherData.name;
-  document.getElementById("weather-type").innerText = weatherData.weather[0].main;
-  let x = weatherData.main.temp;
+  document.getElementById("weather-type").innerText =
+    weatherData.weather[0].main;
+  let x = Math.trunc(((weatherData.main.temp - 32) * 5) / 9);
   document.getElementById("temp").innerText = x;
-  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
-  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
-}
-  
+  document.getElementById("min-temp").innerText =
+    Math.trunc(((weatherData.main.temp_min - 32) * 5) / 9) - 2;
+  document.getElementById("max-temp").innerText = Math.trunc(
+    ((weatherData.main.temp_max - 32) * 5) / 9
+  );
+};
